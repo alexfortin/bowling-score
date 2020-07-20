9c3701a4aa151916c106bfc510ed7a75f6a683d0
Alex Fortin <alexfortin@utexas.edu>
2020-07-19 20:38:59 -0500

    GREEN

--- src/main/java/com/bowling/bowlingscore/resource/BowlingResource.java
+++ src/main/java/com/bowling/bowlingscore/resource/BowlingResource.java
@@ -7,9 +7,13 @@ import org.dozer.DozerBeanMapperBuilder;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
@@ -18,6 +22,12 @@ public class BowlingResource {
     BowlingService bowlingService;
 
     Mapper mapper = DozerBeanMapperBuilder.create().build();
+
+    @GetMapping("/game/{id}")
+    public Game getGame(@PathVariable(value="id") UUID id) {
+        GameEntity gameEntity = bowlingService.getGame(id);
+        return mapper.map(gameEntity, Game.class);
+    }
 
     @PostMapping("/game")
     public Game createGame() {
--- src/main/java/com/bowling/bowlingscore/service/BowlingService.java
+++ src/main/java/com/bowling/bowlingscore/service/BowlingService.java
@@ -5,7 +5,12 @@ import com.bowling.bowlingscore.jpa.FrameRepository;
 import com.bowling.bowlingscore.jpa.GameEntity;
 import com.bowling.bowlingscore.jpa.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Component
 public class BowlingService {
@@ -16,6 +21,11 @@ public class BowlingService {
 
     @Autowired
     FrameRepository frameRepository;
+
+    public GameEntity getGame(UUID id) {
+        Optional<GameEntity> gameEntity = gameRepository.findById(id);
+        return gameEntity.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Game not found", null));
+    }
 
     public GameEntity createGame() {
         GameEntity gameEntity = new GameEntity();
